031eedaa8c4f0bb9a05b1ec31e6a7e53cf18fbe7
Aljoscha Wiesner <aljo0025@easv365.dk>
2020-12-18 18:54:36 +0100

    Cleaned ManagerModel

--- AdminModule/AdminModule/ViewModels/ManageCustomerModel.cs
+++ AdminModule/AdminModule/ViewModels/ManageCustomerModel.cs
@@ -29,15 +29,6 @@ namespace AdminModule.ViewModels
           
         }
 
-        public int UserIDInput { get; set; } 
-        public string PasswordInput { get; set; } 
-        public string AddressInput { get; set; }
-        public int PhoneNumberInput { get; set; }
-        public string EmailInput { get; set; }
-        public int UserStatusIDInput { get; set; } 
-        public string NameInput { get; set; }
-
-
         public ObservableCollection<UserReadDto> Users { get; set; }
         public IIntegrationService Api { get; set; }
 
@@ -87,76 +78,11 @@ namespace AdminModule.ViewModels
             Users.Clear();
             GetTable();
         });
-
-
-        #region TestMethods
-
-        /// <summary>
-        ///     Getting a single entry through the DTO
-        /// </summary>
-        private void GetSingleEntry()
-        {
-            var tagDto = Api.GetSingleEntryById<TagReadDto>("tags", 1);
-            MessageBox.Show("TagID: " + tagDto.TagID + " TagTitle: " + tagDto.Title, "GetSingle test");
-        }
-
-        /// <summary>
-        ///     Getting a table entry through the DTO
-        /// </summary>
         private void GetTable()
         {
             var entries = Api.GetTable<UserReadDto>("users");
             foreach (var tag in entries) Users.Add(tag);
         }
 
-        /// <summary>
-        ///     Getting an insert entry through the DTO
-        /// </summary>
-        private void InsertToTable()
-        {
-            var tagToBeInserted = new TagCreateDto
-                {Title = "this tag has just been inserted"}; //NOT supplying primary key, db will auto increment itself
-            var insertResponse = Api.Insert("tags", tagToBeInserted);
-            MessageBox.Show(insertResponse.ToString());
-        }
-
-        /// <summary>
-        ///     Updating through the DTO
-        /// </summary>
-        private void UpdateTable()
-        {
-            //fetching newly created tag to update it with another title, since i just made it, it has to have the highest id
-            var tagList = Api.GetTable<TagReadDto>("tags");
-            var tagToBeUpdated = tagList.Find(x => x.TagID == tagList.Max(t => t.TagID));
-            if (tagToBeUpdated == null) return;
-
-            //and now update the title
-            var updatingTag = new TagUpdateDto
-            {
-                TagID = tagToBeUpdated.TagID, Title = "this just updated the title"
-            }; //supplying primary key, so db knows which entry to update with rest of attributes
-            var tagUpdateResponse = Api.Update("tags", updatingTag);
-            MessageBox.Show("if this responds a 204: no content, that means it works! \b" + tagUpdateResponse);
-        }
-
-        /// <summary>
-        ///     Deleting via DTO
-        /// </summary>
-        private void DeleteEntries()
-        {
-            //fetching newly created tag's id to delete it
-            var tagCollection = Api.GetTable<TagReadDto>("tags");
-            var tagId = 0;
-            foreach (var tag in tagCollection)
-                if (tag.TagID > tagId)
-                    tagId = tag.TagID;
-            if (tagId < 1) return;
-
-            //and now delete it
-            var tagDeletionResponseMessage = Api.Delete("tags", tagId);
-            MessageBox.Show("if this responds a 204: no content, that means it works! \b" + tagDeletionResponseMessage);
-        }
-
-        #endregion
     }
 }
